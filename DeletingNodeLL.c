c4c64b740ee0385337023a2f092433eb17a7a265
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-06-30 21:00:43 +0530

    Update DeletingNodeLL.c

--- DeletingNodeLL.c
+++ DeletingNodeLL.c
@@ -99,6 +99,28 @@ void delete_end(struct node *h)
     temp->next = NULL;
 }
 
+void deleteNode(struct node** head_ref, int key)
+{
+    
+    struct node *temp = *head_ref, *prev;
+    if (temp != NULL && temp->data == key) {
+        *head_ref = temp->next;
+        free(temp);
+        return;
+    }
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL)
+        return;
+    prev->next = temp->next;
+ 
+    free(temp);
+}
+
+ 
+
 int main()
 {
     struct node *head = NULL;
@@ -116,6 +138,8 @@ int main()
     delete_end(head);
     printlist(head);
     delete_mid(head, 4);
+    printlist(head);
+    deleteNode(&head, 12);
     printlist(head);
 
     return 0;
