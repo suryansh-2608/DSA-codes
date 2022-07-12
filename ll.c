1e32fa0a0ac48235f0f2068a9bab4adf391e1c06
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-07-12 22:42:44 +0530

    Update ll.c

--- ll.c
+++ ll.c
@@ -1,120 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+
 struct node
 {
     int data;
     struct node *next;
-} * first = NULL;
+};
 
-void create(int a[], int n)
+void LinkedListTraversal(struct node *ptr)
 {
-    int i;
-    struct node *t, *last;
-    first = (struct node *)malloc(sizeof(struct node));
-    first->data = a[0];
-    first->next = NULL;
-    last = first;
-
-    for(i=1; i<n; i++)
+    while(ptr!=NULL)
     {
-        t = (struct node *)malloc(sizeof(struct node));
-        t->data = a[i];
-        t->next = NULL;
-        last->next = t;
-        last=t;
-    }
-
-}
-
-void Display(struct node * p)
-{
-    while(p!=NULL)
-    {
-        printf("%d ", p->data);
-        p = p->next;
+        printf("%d\t", ptr->data);
+        ptr = ptr->next;
     }
 }
 
-void count(struct node *p)
-{
-    int count = 0;
-    while(p!=NULL)
-    {
-        count ++;
-        p=p->next;
-        
-
-    }
-    printf("\n%d", count);
-}
-
-int sum(struct node *p)
-{
-    int sum = 0;
-    while(p!=NULL)
-    {
-        sum += p->data;
-        p = p->next;
-    }
-    printf("\n%d", sum);
-}
-
-int max(struct node *p)
-{
-    int m = INT_MIN;
-    while(p!=NULL)
-    {
-        if(m<p->data)
-            m = p->data;
-        p=p->next;
-    }
-    printf("\n%d", m);
 
 
-}
-
-int min(struct node *p)
-{
-    int m = INT_MAX;
-    while(p!=NULL)
-    {
-        if(m>p->data)
-            m = p->data;
-        p=p->next;
-    }
-    printf("\n%d", m);
-
-
-}
-
-void search(struct node * p, int key)
-{
-    struct node *q;
-
-    while(p!=NULL)
-    {
-        if(key == p->data)
-        {
-            q->next = p->next;
-            p->next = first;
-            first = p;
-            printf("\nKey found!!!!!!!");
-        }
-        q=p;
-        p = p->next;
-    }
-}
 
 int main()
 {
-    int A[] = {3, 5, 7, 10, 15};
-    create(A, 5);
-    Display(first);
-    count(first);
-    sum(first);
-    max(first);
-    min(first);
-    search(first, 5);
+    struct node *head;
+    struct node *second;
+    struct node *third;
+
+
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+
+
+    head->data = 7;
+    head->next = second;
+
+
+    second->data = 13;
+    second->next = third;
+
+
+    third->data = 67;
+    third->next = NULL; 
+
+    LinkedListTraversal(head);
+    
     return 0;
 }
