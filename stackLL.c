7b9ffe3df5c08aaff2bb4cdc8cfe040d4e4990d1
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-07-20 09:20:36 +0530

    Update stackLL.c

--- stackLL.c
+++ stackLL.c
@@ -9,12 +9,32 @@ struct stack
 
 struct stack *push(struct stack *s, int data)
 {
+    struct stack *n = (struct stack *)malloc(sizeof(struct stack));
+    if(n==NULL)
+        printf("Stack Overflow\n");
+    else
+    {
+        n->data = data;
+        n->next = s;
+        s = n;
+        return s;
+    }
 
 }
 
 struct stack *pop(struct stack *s)
 {
-
+    struct stack * n = s;
+    if(s==NULL)
+        printf("The stack is already empty\n");
+    else
+    {
+        s = s->next;
+        
+        int x = n->data;
+        free(n);
+        printf("The popped element is %d\n", x);
+    }
 }
 
 void isEmpty(struct stack * s)
@@ -35,16 +55,32 @@ void isFull(struct stack *s)
     if(n == NULL)
         printf("The stack is FULL\n");
     else
-        printf("The stack is not FULL");
+        printf("The stack is not FULL\n");
 }
 
-void peek(struct stack *s, int data)
+void peek(struct stack *s)
 {
-
+    while(s != NULL)
+    {
+        printf("%d\t", s->data);
+        s = s->next;
+    }
 }
 
 int main()
 {
-    struct stack * s;
+    struct stack * s = NULL;
+    isEmpty(s);
+    s = push(s, 11);
+    s = push(s, 12);
+    s = push(s, 13);
+    s = push(s, 14);
+    s = push(s, 15);
+    s = push(s, 16);
+    s = push(s, 17);
+    isFull(s);
+    isEmpty(s);
+    peek(s);
+
     return 0;
 }
