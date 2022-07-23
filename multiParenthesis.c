e9f3e801456cdc71b68282f4a0c764667c96171d
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-07-23 09:13:47 +0530

    Update multiParenthesis.c

--- multiParenthesis.c
+++ multiParenthesis.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+ 
 struct stack
 {
     int size;
     int top;
     char *arr;
 };
-
+ 
 int isEmpty(struct stack *ptr)
 {
     if (ptr->top == -1)
@@ -53,11 +53,66 @@ char pop(struct stack* ptr){
         return val;
     }
 }
-
-
+ 
+char stackTop(struct stack* sp){
+    return sp->arr[sp->top];
+}
+ 
+int match(char a, char b){
+    if(a=='{' && b=='}'){
+        return 1;
+    }
+    if(a=='(' && b==')'){
+        return 1;
+    }
+    if(a=='[' && b==']'){
+        return 1;
+    }
+  return 0;  
+}
+ 
+int parenthesisMatch(char * exp){
+    // Create and initialize the stack
+    struct stack* sp;
+    sp->size = 100;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char));
+    char popped_ch;
+ 
+    for (int i = 0; exp[i]!='\0'; i++)
+    {
+        if(exp[i]=='(' || exp[i]=='{' || exp[i]=='['){
+            push(sp, exp[i]);
+        }
+        else if(exp[i]==')'|| exp[i]=='}' || exp[i]==']'){
+            if(isEmpty(sp)){
+                return 0;
+            }
+            popped_ch = pop(sp);
+            if(!match(popped_ch, exp[i])){ 
+              return 0;  
+            }
+        }
+    }
+ 
+    if(isEmpty(sp)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    
+}
+ 
 int main()
 {
-    char * exp = " ";
-    parenthesisMatch(exp);
+    char * exp = "[12]";
+    
+    if(parenthesisMatch(exp)){
+        printf("The parenthesis is balanced");
+    }
+    else{
+        printf("The parenthesis is not balanced");
+    }
     return 0;
 }
