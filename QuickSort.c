3ba1dd6f7d2b6d422da3ed980d0476590efa9d0d
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-07-24 16:47:46 +0530

    Update QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -9,19 +9,57 @@ void printArray(int* a, int n)
     printf("\n");
 }
 
-void QuickSort(int *a, int n)
+int partition(int *a, int low, int high)
 {
-    printf("Quick Sort is Running.....");
-    
-    
+    int pivot = a[low];
+    int i = low+1;
+    int j = high;
+    do{
+    while(a[i] <= pivot)
+    {
+        i++;
+    }
+
+    while(a[j]>pivot)
+    {
+        j--;
+    }
+
+    if(i<j)
+    {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    }while(i<j);
+
+    int temp = a[low];
+    a[low] = a[j];
+    a[j] = temp;
+
+    return j;
+
+}
+
+void QuickSort(int *a, int low, int high)
+{
+    int partitionIndex;
+    if(low<high)
+    {
+        partitionIndex = partition(a, low, high);
+
+        QuickSort(a, low, partitionIndex-1);
+        QuickSort(a, partitionIndex+1, high);
+    }
+
 }
 
 int main()
 {
-    int a[] = {12, 54, 65, 7, 23, 9};
-    int n = 6;
+    int a[] = {3, 5, 2, 13, 12};
+    int n = 5;
     printArray(a, n);
-    QuickSort(a, n);
+    QuickSort(a, 0, n-1);
     printArray(a, n);
     return 0;
 }
