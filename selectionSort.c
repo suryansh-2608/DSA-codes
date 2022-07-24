4e12b14e3c9e871e62e989e08bda19e5a9ebe4e2
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-07-24 16:33:14 +0530

    Update selectionSort.c

--- selectionSort.c
+++ selectionSort.c
@@ -12,13 +12,28 @@ void printArray(int* a, int n)
 
 void selectionSort(int *a, int n)
 {
-    
+    printf("Running Selection Sort....\n");
+    for(int i = 0; i<n-1; i++)
+    {
+        int indexOfMin = i;
+        for(int j = i+1; j<n; j++)
+        {
+            if(a[j]<a[indexOfMin])
+            {
+                indexOfMin = j;
+            }
+
+        }
+        int temp = a[i];
+        a[i] = a[indexOfMin];
+        a[indexOfMin] = temp;
+    }
 }
 
 int main()
 {
-    int a[] = {12, 54, 65, 7, 23, 9};
-    int n = 6;
+    int a[] = {3, 5, 2, 13, 12};
+    int n = 5;
     printArray(a, n);
     selectionSort(a, n);
     printArray(a, n);
