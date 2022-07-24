6b7e54ba1c721721809def5b7f93d4c32e5a5396
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-07-24 17:11:47 +0530

    Update countSort.c

--- countSort.c
+++ countSort.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
 
 void printArray(int* a, int n)
 {
@@ -9,12 +11,63 @@ void printArray(int* a, int n)
     printf("\n");
 }
 
+int maximum(int *a, int n)
+{
+    int max = INT_MIN;
+    for(int i = 0; i<n; i++)
+    {
+        if(a[i]>max)
+        {
+            max = a[i];
+        }
+    }
+    return max;
+
+
+}
+
+void CountSort(int *a, int n)
+{
+    int i, j;
+    int max = maximum(a, n);
+    int * count = (int *)malloc((max+1) * sizeof(int));
+    for ( i = 0; i < max+1; i++)
+    {
+       count[i] = 0;
+    }
+
+    for ( i = 0; i < n; i++)
+    {
+        count[a[i]] = count[a[i]] + 1;
+    }
+
+    i = 0;
+    j = 0;
+    
+    while (i<=max)
+    {
+        if(count[i]>0)
+        {
+            a[j] = i;
+            count[i] = count[i] - 1;
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    
+    
+    
+}
+
 int main()
 {
-    int a[] = {9, 14, 4, 8, 7, 5, 6};
+    int a[] = {3, 1, 9, 7, 1, 2, 4};
     int n = 7;
     printArray(a, n);
-    MergeSort(a, 0, n - 1);
+    CountSort(a, n);
     printArray(a, n);
     return 0;
 }
